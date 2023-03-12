b369be9180252cc9cf5835bd78466efd0e5c578b
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-12 18:59:54 -0400

    feat(controller/category): controller updateCategories added

--- src/controller/category.js
+++ src/controller/category.js
@@ -50,11 +50,49 @@ exports.addCategory = (req, res) => {
 exports.getCategories = (req, res) => {
    Category.find({}).exec((error, categories) => {
       if (error) return res.status(400).json({ error });
-
       if (categories) {
          const categoryList = createCategories(categories);
-
          res.status(200).json({ categoryList });
       }
    });
+};
+
+exports.updateCategory = async (req, res) => {
+   const { _id, name, parentId, type } = req.body;
+   const updatedCategories = [];
+
+   if (name instanceof Array) {
+      for (let i = 0; i < name.length; i++) {
+         const category = {
+            name: name[i],
+            type: type[i],
+         };
+         if (parentId[i] !== "") {
+            category.parentId = parentId[i];
+         }
+
+         const updateCategory = await Category.findOneAndUpdate(
+            { _id: _id[i] },
+            category,
+            { new: true }
+         );
+         updatedCategories.push(updateCategory);
+      }
+      console.log(updatedCategories);
+      res.status(201).json({ updatedCategories });
+   } else {
+      const category = {
+         name,
+         type,
+      };
+      if (parentId !== "") {
+         category.parentId = parentId;
+      }
+      const updatedCategories = await Category.findOneAndUpdate(
+         { _id },
+         category,
+         { new: true }
+      );
+      res.status(201).json({ updatedCategories });
+   }
 };
