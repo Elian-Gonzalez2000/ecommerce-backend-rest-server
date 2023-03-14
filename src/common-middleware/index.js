f0dad74b497f631a5e38db2f2806de60d407adfb
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-13 23:58:47 -0400

    refactor(common-middleware/index) change the status server sended 400 to 500

--- src/common-middleware/index.js
+++ src/common-middleware/index.js
@@ -17,14 +17,14 @@ exports.requiresSignin = (req, res, next) => {
 
 exports.userMiddleware = (req, res, next) => {
    if (req.user.role !== "user") {
-      return res.status(400).json({ message: "User access denied" });
+      return res.status(500).json({ message: "User access denied" });
    }
    next();
 };
 
 exports.adminMiddleware = (req, res, next) => {
    if (req.user.role !== "admin") {
-      return res.status(400).json({ message: "Admin access denied" });
+      return res.status(500).json({ message: "Admin access denied" });
    }
    next();
 };
