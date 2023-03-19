d5506c057d0a1a5f03280a664f7eb156cd8c162f
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-18 21:20:43 -0400

    feat(/router/admin/page): Add middlewares

--- src/router/admin/page.js
+++ src/router/admin/page.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const { upload, createPage } = require("../../common-middleware/index.js");
+const {
+   upload,
+   adminMiddleware,
+   requiresSignin,
+} = require("../../common-middleware/index.js");
+const { createPage } = require("../../controller/admin/page.js");
 const router = express.Router();
 
 router.post(
    "/page/create",
+   requiresSignin,
+   adminMiddleware,
    upload.fields([
-      ({ name: "banners", maxCount: 5 }, { name: "products", maxCount: 5 }),
+      { name: "banners", maxCount: 5 },
+      { name: "products", maxCount: 5 },
    ]),
    createPage
 );
