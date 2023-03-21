4e3f2d055c8730e660a29237b73f867806313011
Elian Gonzalez <eliancarlogm@gmail.com>
2023-03-21 12:17:28 -0400

    feat(controller/admin/page): Add controller getPage

--- src/controller/admin/page.js
+++ src/controller/admin/page.js
@@ -9,7 +9,7 @@ exports.createPage = (req, res) => {
          nabigateTo: `/bannerClicked?categoryId=${req.body.category}&type=${req.body.type}`,
       }));
    }
-   if (products.length > 0) {
+   if (products && products.length > 0) {
       req.body.products = products.map((product, index) => ({
          img: `${process.env.API}/public/${product.filename}`,
          nabigateTo: `/productClicked?categoryId=${req.body.category}&type=${req.body.type}`,
@@ -18,12 +18,36 @@ exports.createPage = (req, res) => {
 
    req.body.createdBy = req.user._id;
 
-   const page = new Page(req.body);
-
-   page.save((error, page) => {
-      if (error) res.status(400).json({ error });
+   Page.findOne({ category: req.body.category }).exec((error, page) => {
+      if (error) return res.status(400).json({ error });
       if (page) {
-         return res.status(201).json({ page });
+         Page.findOneAndUpdate({ category: req.body.category }, req.body).exec(
+            (error, updatedPage) => {
+               if (error) return res.status(400).json({ error });
+               if (updatedPage) {
+                  return res.status(201).json({ page: updatedPage });
+               }
+            }
+         );
+      } else {
+         const page = new Page(req.body);
+
+         page.save((error, page) => {
+            if (error) res.status(400).json({ error });
+            if (page) {
+               return res.status(201).json({ page });
+            }
+         });
       }
    });
+};
+
+exports.getPage = (req, res) => {
+   const { category, type } = req.params;
+   if (type === "page") {
+      Page.findOne({ category: category }).exec((error, page) => {
+         if (error) return res.status(400).json({ error });
+         if (page) return res.status(201).json({ page });
+      });
+   }
 };
