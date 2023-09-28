72d49d4024eff0c206256b371fd399c4ff743b74
Elian Gonzalez <eliancarlogm@gmail.com>
2023-09-28 19:42:57 -0400

    fix(src/router/admin/order.routes.js): fix wrong import name requiresSignin

--- src/router/admin/order.routes.js
+++ src/router/admin/order.routes.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const { requireSignin, adminMiddleware } = require("../../common-middleware");
-const { updateOrder } = require("../../controller/admin/order.admin");
+const {
+   requiresSignin,
+   adminMiddleware,
+} = require("../../common-middleware/index.js");
+const { updateOrder } = require("../../controller/admin/order.admin.js");
 const router = express.Router();
 
-router.post(`/order/update`, requireSignin, adminMiddleware, updateOrder);
+router.post(`/order/update`, requiresSignin, adminMiddleware, updateOrder);
 
 module.exports = router;
