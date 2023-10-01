666916a8005c6fa076570d766fe3e055dfd425e4
Elian Gonzalez <eliancarlogm@gmail.com>
2023-10-01 18:12:02 -0400

    feat(src/router/order.js): add route for getOrder controller

--- src/router/order.js
+++ src/router/order.js
@@ -2,10 +2,11 @@ const {
    requiresSignin,
    userMiddleware,
 } = require("../common-middleware/index.js");
-const { addOrder, getOrders } = require("../controller/order.js");
+const { addOrder, getOrders, getOrder } = require("../controller/order.js");
 const router = require("express").Router();
 
 router.post("/add-order", requiresSignin, userMiddleware, addOrder);
 router.get("/get-orders", requiresSignin, userMiddleware, getOrders);
+router.post("/get-order", requiresSignin, userMiddleware, getOrder);
 
 module.exports = router;
