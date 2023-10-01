7b70adc37b85f06d9822a5cdf1b8a1f2b5ec196b
Elian Gonzalez <eliancarlogm@gmail.com>
2023-10-01 17:50:08 -0400

    src/router/admin/order.routes.js

--- src/router/admin/order.routes.js
+++ src/router/admin/order.routes.js
@@ -3,9 +3,18 @@ const {
    requiresSignin,
    adminMiddleware,
 } = require("../../common-middleware/index.js");
-const { updateOrder } = require("../../controller/admin/order.admin.js");
+const {
+   updateOrder,
+   getCustomerOrders,
+} = require("../../controller/admin/order.admin.js");
 const router = express.Router();
 
 router.post(`/order/update`, requiresSignin, adminMiddleware, updateOrder);
+router.post(
+   `/order/get-customer-orders`,
+   requiresSignin,
+   adminMiddleware,
+   getCustomerOrders
+);
 
 module.exports = router;
